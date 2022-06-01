7d07b4700c1fd83b8a24297f87c307a770be4432
rodiond26 <51717975+rodiond26@users.noreply.github.com>
2022-06-01 23:49:27 +0500

    Add files via upload

---
+++ comfunc.go
@@ -0,0 +1,6 @@
+package go_test_package
+
+// Sum of two integer numbers
+func Sum(num1, num2 int) int {
+	return num1 + num2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rodiond26/go_test_package
+
+go 1.18
